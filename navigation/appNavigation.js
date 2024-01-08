d91ea09ee2b86480860a69ef786bcf1d4d78e913
mohsensami <mohsensami88@gmail.com>
2024-01-08 20:41:38 +0330

    install navigation and nativewind

---
+++ navigation/appNavigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import AboutScreen from '../screens/AboutScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+                <Stack.Screen name="About" options={{ headerShown: false }} component={AboutScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function HomeScreen() {
+    return (
+        <View className="flex-1 items-center justify-center bg-red-500">
+            <Text>HomeScreen</Text>
+        </View>
+    );
+}
